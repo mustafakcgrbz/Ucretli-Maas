57f1f2e9a757858190ffc2f7006c5d87bf92bb90
Mustafa <mustafakcgrbz@gmail.com>
2017-06-12 00:04:09 +0300

    Sunum Hali

--- Ücretli Maaş/Program.cs
+++ Ücretli Maaş/Program.cs
@@ -16,7 +16,7 @@ namespace Ücretli_Maaş
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmAna());
+            Application.Run(new FrmGiris());
         }
     }
 }
